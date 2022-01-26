b438b9fb420816238f8662af62edea842b58f32f
vaibhav301 <60523994+vaibhav301@users.noreply.github.com>
2022-01-26 12:34:43 +0530

    Create q1. Maximum_profit.cpp

---
+++ milestone-2(microsoft)/q1. Maximum_profit.cpp
@@ -0,0 +1,29 @@
+class Solution {
+  public:
+    int maxProfit(int K, int N, int A[]) {
+        // code here
+        int dp[K+1][N];
+        for(int i=0; i<N; i++)
+        dp[0][i] = 0;
+        for(int i=1; i<K+1; i++)
+        dp[i][0]=0;
+        
+        for(int i=1; i<K+1; i++)
+        { 
+        
+            int temp=INT_MIN;
+            for(int j=1; j<N; j++)
+            {
+              
+                {
+                    if(A[j]-A[k]>=0)
+                    temp=max(temp, A[j]-A[k]+dp[i-1][k]);
+                } 
+                
+                temp=max(temp, dp[i-1][j-1]-A[j-1]);
+                dp[i][j]=max(dp[i][j-1], temp+A[j]);
+            }
+        }
+        return dp[K][N-1];
+    }
+};
